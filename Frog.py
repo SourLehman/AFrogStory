4603eb7a7a081cc3570a48621a333c4e3a0be641
SourLehman <105559179+SourLehman@users.noreply.github.com>
2022-05-14 10:02:09 -0700

    added_hello
    added print("hello world") to level.py

---
+++ Frog.py
@@ -0,0 +1,36 @@
+# Frog Game
+# Matt Rilling
+# 5-1-2022
+
+import pygame
+import sys
+from settings import *
+from level import Level
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+        pygame.display.set_caption('Frog')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
